b91ce5d7e1e6d4f38bf652beaa36e43551c52941
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-06-21 15:34:05 -0300

    finished class 11

---
+++ cursoJavaScript/aula04/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 78.2
+console.log(`A velocidade do seu carro é ${velocidade}km/h`)
+if(velocidade > 60){ // condição simples
+    console.log(`Você ultrapasou a velocidade permitida. MULTADO!`)
+}
+console.log(`Diriga sempre usando cinto de segurança!`)
