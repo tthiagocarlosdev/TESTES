61203fd784359d828e096b03d62b349e6a09cfb0
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-06-23 17:53:47 -0300

    finished exercise

--- cursoJavaScript/aula04/ex018/script.js
+++ cursoJavaScript/aula04/ex018/script.js
@@ -1,4 +1,19 @@
+
 function contar(){
+    function contador(){
+        resposta.innerHTML = `<p>Contando....\u{1F914}</p>`
+        if(inicio < fim){
+            for(let contador = inicio; contador <= fim; contador += passo){
+                resposta.innerHTML += `${contador} \u{1F449} `
+            }
+        } else if (inicio > fim){
+            for (let contador = inicio; contador >= fim; contador -= passo){
+                resposta.innerHTML += `${contador} \u{1F449} `
+            }
+        }
+        resposta.innerHTML += `\u{1F3C1}`
+    }
+
     var resposta = document.querySelector('div#resposta')
     var inicio = document.querySelector('input#inicio')
     var fim = document.querySelector('input#fim')
@@ -6,26 +21,15 @@ function contar(){
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         resposta.innerHTML = `[ERROR] Impossível contar!`
     } else {
-        if(Number(passo.value) == 0){
-            passo = 1
-            alert('Passo inválido! Considerando passo 1')
-        }
         inicio = Number(inicio.value)
         fim = Number(fim.value)
         passo = Number(passo.value)
-        resposta.innerHTML = `<p>Contando....</p>`
-        if(inicio < fim){
-            for(let contador = inicio; contador <= fim; contador += passo){
-                resposta.innerHTML += `${contador} -> `
-            }
-        } else if (inicio > fim){
-            for (let contador = inicio; contador >= fim; contador -= passo){
-                resposta.innerHTML += `${contador} -> `
-            }
-
+        if(passo <= 0){
+            passo = 1
+            alert('Passo inválido! Considerando passo 1')
+            contador()
+        } else {
+            contador()
         }
-        resposta.innerHTML += `FIM`
-        
-    }
-    
+    }   
 }
