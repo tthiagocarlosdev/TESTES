90e78c73bc3e41057a4b72cfc93a73f5468992ee
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-09-09 07:40:55 -0300

    modified randomStringGenerator

--- randomStringGenerator/randomStringGenerator.js
+++ randomStringGenerator/randomStringGenerator.js
@@ -7,27 +7,32 @@ function header(){
     console.log("=========================")
 }
 
-//random number generator function
-function getRandomIntInclusive(min, max){
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1)) + min
+//FUNCTION RANDOM STRING GENERATOR
+function randomStringGenerator(stringSize){
+    //random number generator function
+    function getRandomIntInclusive(min, max){
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        return Math.floor(Math.random() * (max - min + 1)) + min
+    }
+
+    //variable declaration
+    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    //var stringSize = Number(readlineSync.question("Digite o tamanho da string: "))
+    var randomString = ""
+
+    //calculations with charAt method and number generator function
+    for(counter = 1; counter <= stringSize; counter++){
+        randomString += characters.charAt(getRandomIntInclusive(0, 52))
+    }
+
+    return randomString
 }
 
+//program
 header()
-
-//variable declaration
-var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-var stringSize = Number(readlineSync.question("Digite o tamanho da string: "))
-var String = ""
-
-//calculations with charAt method and number generator function
-for(counter = 1; counter <= stringSize; counter++){
-    String += characters.charAt(getRandomIntInclusive(0, 52))
-}
-
-//final program report
+var stringSize = randomStringGenerator(Number(readlineSync.question("Digite o tamanho da string: ")))
 console.clear()
 header()
-console.log(`Generated string: ${String}`)
+console.log(`Generated string: ${stringSize}`)
 console.log("=========================")
