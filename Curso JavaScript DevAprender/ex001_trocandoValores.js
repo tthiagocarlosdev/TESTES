2c233fd73fdee80040b1575b4f9902d849b24fc2
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-09-15 18:49:10 -0300

    Curso JavaScript DevAprender 1ºs exercicios

---
+++ Curso JavaScript DevAprender/ex001_trocandoValores.js
@@ -0,0 +1,20 @@
+console.log('----------------')
+console.log('TROCANDO VALORES')
+console.log('----------------')
+
+let a = "vermelho"
+let b = 'azul'
+
+console.log('Valor Original!')
+console.log(`Valor de a: ${a}`)
+console.log(`Valor de b: ${b}`)
+
+let x = a
+a = b
+b = x
+
+console.log('----------------')
+console.log("Após troca!")
+console.log(`Valor de a: ${a}`)
+console.log(`Valor de b: ${b}`)
+console.log('----------------')
---
+++ Curso JavaScript DevAprender/index.js
@@ -0,0 +1,37 @@
+var readlineSync = require('readline-sync')
+/*var name = readlineSync.question('Whats is your name? ')
+var name = "Thiago Carlos"
+console.log(`Welcome ${name}!`)
+let pessoa = {
+    nome: 'Silva',
+    idade: 33
+}
+console.log(`Meu nome é ${pessoa.nome}!`)
+console.log(`Minha idade: ${pessoa.idade} anos!`)
+
+// Arrays
+console.log("Arrays")
+let familia =[true, 45, 'jhonatan', 17]
+console.log(familia)
+
+
+// Operador tenário
+var idade = readlineSync.question("Digite sua idade: ")
+let maioridade = idade >= 18 ? 'Maior de idade!' : 'Menor de idade!'
+console.log("===================")
+console.log(maioridade)
+
+for(let i = 0; i < 5; i++){
+    console.log("Estou aprendendo!")
+}
+
+
+const cores = ['Vermelho', 'Azul', 'Verde']
+for(let cor in cores){
+    console.log(cor, cores[cor])
+}
+
+const cores = ['Vermelho', 'Azul', 'Verde']
+for(let cor of cores){
+    console.log(cor)
+}*/
