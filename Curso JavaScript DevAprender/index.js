b86d5c93a0fb7494f1c2243d578e557df30fb2cb
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-09-22 08:01:46 -0300

    Curso JavaScript DevAprender Factory Functions and Constructor Functions

--- Curso JavaScript DevAprender/index.js
+++ Curso JavaScript DevAprender/index.js
@@ -34,4 +34,67 @@ for(let cor in cores){
 const cores = ['Vermelho', 'Azul', 'Verde']
 for(let cor of cores){
     console.log(cor)
-}*/
+}
+
+
+//factoryFunctions → Funções de Fábrica
+console.log('---------------------------------------')
+console.log(' factoryFunctions → Funções de Fábrica ')
+console.log('---------------------------------------')
+function criarCelular(marcaCelular, tamanhoTela, capaciadeBateria){
+    return {
+        marcaCelular,
+        tamanhoTela,
+        capaciadeBateria,
+        ligar(){
+            console.log('Fazendo ligação...')
+        }
+    }
+}
+let marca = readlineSync.question('Qual a marca do celular: ')
+let tamanho = Number(readlineSync.question('Qual o tamanho da tela: '))
+let bateria = Number(readlineSync.question('Qual a capacidade da bateria: '))
+console.log(criarCelular(marca, tamanho, bateria))
+console.log('---------------------------------------')
+
+//Constructor Functions → Função de Construtor
+
+console.log('----------------------------------------------')
+console.log(' Constructor Functions → Função de Construtor ')
+console.log('----------------------------------------------')
+function Celular(marcaCelular, tamanhoTela, capacidadeBateria){
+    this.marcaCelular = marcaCelular,
+    this.tamanhoTela = tamanhoTela,
+    this.capacidadeBateria = capacidadeBateria,
+    this.ligar = function(){
+        console.log('fazendo ligação...')
+    }
+}
+let marca = readlineSync.question('Qual a marca do celular: ')
+let tamanho = Number(readlineSync.question('Qual o tamanho da tela: '))
+let bateria = Number(readlineSync.question('Qual a capacidade da bateria: '))
+console.log(new Celular(marca, tamanho, bateria))
+console.log('----------------------------------------------')
+*/
+
+//Natureza dinâmica de Objetos
+console.log('Objeto original:')
+const mouse = {
+    cor: 'red',
+    marcar: 'dazz',
+}
+console.log(mouse)
+
+console.log('-------------------')
+console.log('Adicionando Propriedades:')
+mouse.velocidade = 5000
+mouse.trocarDPI = function () {
+    console.log('mudando DPI')
+}
+console.log(mouse)
+
+console.log('-------------------')
+console.log('Deletando Propriedades:')
+delete mouse.velocidade
+delete mouse.trocarDPI
+console.log(mouse)
