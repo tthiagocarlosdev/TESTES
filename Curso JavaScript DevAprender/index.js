913059387179f5d2a0804ac5bf07826fcf5e2ad8
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-10-05 12:20:31 -0300

    Introdução a Arrays: Adicionar novos elementos; Encontrar elementos; Remover elementos; Dividir elementos; Dividir Arrays; Combinar Arrays.

--- Curso JavaScript DevAprender/index.js
+++ Curso JavaScript DevAprender/index.js
@@ -182,7 +182,7 @@ Para acompanhar os vídeos não se esqueça de ativar o sino.
 
 Obrigago,
 Bill.`
-console.log(email)*/
+console.log(email)
 
 // Date
 
@@ -194,4 +194,125 @@ const data3 = new Date(2022,08,27,7,30)
 console.log(data3)
 console.log(data1.toDateString()) // para apresentar como string.
 console.log(data1.toTimeString()) // para apresentar o padrão.
-console.log(data1.toISOString()) // como visualiza no banco de dados.
+console.log(data1.toISOString()) // como visualiza no banco de dados.*/
+
+// INTRODUÇÃO A ARRAYS
+/*
+- Adicionar novos elementos;
+- Encontrar elementos;
+- Remover elementos;
+- Dividir elementos;
+- Dividir Arrays;
+- Combinar Arrays.
+
+
+// ADICIONAR ELEMENTOS A UM ARRAY
+let numeros = [1, 2, 3]
+console.log(`Array Original: ${numeros}`)
+// Início
+numeros.unshift(0)
+console.log(`Array add início: ${numeros}`)
+// Meio
+numeros.splice(1, 0, 'a') // o 1ª parâmetro é o índice onde será add o novo valor. O 2º parâmetro é o que você quer deletar e o 3º é o que será inserido.
+console.log(`Array add no meio: ${numeros}`)
+// Final
+numeros.push(5)
+console.log(`Array add final: ${numeros}`)
+
+// ENCONTRANDO ELEMENTOS (PRIMITIVOS)
+// indexOf -> retorna o índice do valor procurado dentro do array. Caso o valor não seja encontrado, será retornado -1.
+const numeros = [1, 2, 3, 1, 4]
+console.log(numeros.indexOf(1))
+// lastIndexOf -> retorna o índice da última ocorrência daquele valor procurado dentro do array.
+console.log(numeros.lastIndexOf(1))
+// .includes -> retorna true ou false se o valor procurado existir dentro do array.
+console.log(numeros.includes(2))
+
+// ENCONTRANDO ELEMENTOS (REFERÊNCIA)
+const marcas =[
+    {id:1, nome: 'a'},
+    {id:2, nome: 'b'}
+]
+const marca = marcas.find(function(marca){
+    return marca.nome === 'b'
+})
+console.log(marca)
+
+// ARROW FUNCTIONS
+
+const marcas = [
+    {id:1, nome: 'a'},
+    {id:2, nome: 'b'}
+]
+
+const marca = marcas.find(marca => marca.nome === 'c')
+console.log(marca)
+
+// REMOVENDO ELEMENTOS DO ARRAY
+
+const alfabeto = ['a', 'b', 'c', 'd', 'e', 'f']
+console.log(`Array Original: ${alfabeto}`)
+
+// Final
+const ultimo = alfabeto.pop()
+console.log(`Último elemento: ${ultimo}`)
+console.log(`Após remover último elemento: ${alfabeto}`)
+// Início
+const primeiro = alfabeto.shift()
+console.log(`Primeiro elemento: ${primeiro}`)
+console.log(`Após remover 1º elemento: ${alfabeto}`)
+// Meio
+const meio = alfabeto.splice(2,1)
+console.log(`Elemento do meio: ${meio}`)
+console.log(`Após remover elemento do meio: ${alfabeto}`)
+
+// ESVAZIANDO UM ARRAY
+
+let letras = ['a', 'b', 'c', 'd', 'e', 'f']
+let outras = letras
+console.log(`Array letras: ${letras}`)
+console.log(`Array outras: ${outras}`)
+
+// 1ª solução:
+// letras = []
+// console.log('1º solução:')
+// console.log(`Array letras: ${letras}`)
+// console.log(`Array outras: ${outras}`)
+
+//2ª solução:
+letras.length = 0
+console.log('2º solução:')
+console.log(`Array letras: ${letras}`)
+console.log(`Array outras: ${outras}`)
+
+// 3ª solução:
+// letras.splice(0, letras.length)
+// console.log('3º solução:')
+// console.log(`Array letras: ${letras}`)
+// console.log(`Array outras: ${outras}`)
+
+// 4ª solução:
+// while(letras.length > 0){
+//     letras.pop()
+// }
+// console.log('4º solução:')
+// console.log(`Array letras: ${letras}`)
+// console.log(`Array outras: ${outras}`)
+*/
+
+// COMBINANDO E CORTANDO ARRAYS
+
+const primeiro = ['a', 'b', 'c']
+const segundo = ['d', 'e', 'f']
+const combinado = primeiro.concat(segundo)
+console.log(`1º array: ${primeiro}`)
+console.log(`2º array: ${segundo}`)
+console.log(`Arrays juntos: ${combinado}`)
+// const cortado = combinado.slice(1, 3)
+// console.log(`Array a partir do índice 1 até o índice 2: ${cortado}`)
+
+// const cortado = combinado.slice(1) // inicia a partir do índice passado até o último índice
+// console.log(`Array a partid do índice passado: ${cortado}`)
+
+const cortado = combinado.slice() // copia todo o array
+console.log(cortado)
