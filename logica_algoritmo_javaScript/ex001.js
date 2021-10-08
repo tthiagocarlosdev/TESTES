f9f119849f0b143a004b659715741d7f6fb93810
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-10-08 14:49:55 -0300

    reorganozando repositorio logica_algoritmo_javaScript

--- logica_algoritmo_javaScript/ex001.js
+++
@@ -1,4 +0,0 @@
-var readlineSync = require('readline-sync')
-var nome = String(readlineSync.question("Olá, qual é o seu nome? "))
-console.clear()
-console.log(`Muito prazer ${nome}, seja bem-vindo!\n`)
--- logica_algoritmo_javaScript/ex002.js
+++
@@ -1,6 +0,0 @@
-var readlineSync = require('readline-sync')
-var number1 = Number(readlineSync.question("Digite o primeiro número: "))
-console.clear()
-var number2 = Number(readlineSync.question("Digite o segundo número: "))
-console.clear()
-console.log(`A soma entre ${number1} e ${number2} é igual a ${number1 + number2}`)
