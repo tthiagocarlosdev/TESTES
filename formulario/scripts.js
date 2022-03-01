678bfeca707665b766cfd16fdb91f690931776e7
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2022-03-01 14:43:57 -0300

    form finished form

--- formulario/scripts.js
+++ formulario/scripts.js
@@ -11,15 +11,50 @@ form.addEventListener('submit', (event) => {
 })
 
 function checkInputs(){
+  
   const userNameValue = userName.value
   const emailValue = email.value
   const passwordValue = password.value
   const passwordConfirmationValue = passwordConfirmation.value
+  const minimumCharactersInPassword = 5
+
 
   if (userNameValue === ''){
-    setErrorFor(userName, 'O nome de usuário é obrigatório!')
+    setErrorFor(userName, 'O nome do usuário é obrigatório!')
   } else {
     setSuccessFor(userName)
+  }
+
+  if(emailValue === ''){
+    setErrorFor(email, 'O e-mail do usuário é obrigatório!')
+  } else {
+    setSuccessFor(email)
+  }
+
+  if(passwordValue === ''){
+    setErrorFor(password, 'A senha do usuário é obrigatória!')
+  } else if ( passwordValue.length <= minimumCharactersInPassword) {
+    setErrorFor(password, 'A senha deve ter no mínimo 6 caracteres.')
+  } else {
+    setSuccessFor(password)
+  }
+
+  if(passwordConfirmationValue === ''){
+    setErrorFor(passwordConfirmation, 'É obrigatório confirmar a senha!')
+  } else if (passwordConfirmationValue !== passwordValue) {
+    setErrorFor(passwordConfirmation, 'As senhas não conferem!')
+  } else {
+    setSuccessFor(passwordConfirmation)
+  }
+
+  const formControls = form.querySelectorAll('.form-control')
+
+  const formIsValid = [...formControls].every((formControl) => {
+    return formControl.className === "form-control success"
+  })
+
+  if(formIsValid){
+    alert('O fomulário está 100% válido!')
   }
 }
 
@@ -36,6 +71,10 @@ function setErrorFor(input, message){
 
 function setSuccessFor(input, message){
   const formControl = input.parentElement
+  const small = formControl.querySelector('small')
+
+  // Add message success
+  small.innerText = ''
 
   // Add class success
   formControl.className = 'form-control success'
---
+++ formulario/teste.js
@@ -0,0 +1,8 @@
+
+
+// for(let category of personalDataValue){
+//   for(let item of category){
+//     console.log(item)
+//   }
+  
+// }
