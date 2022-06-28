20d52b7628cf130e145ba9ef9958bf60bf52aad1
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2022-06-28 14:00:32 -0300

    Site Timer Kettlebell: trying to add stopwatch

--- site_timekettlebell/time_kettlebell/src/pages/Timer.jsx
+++ site_timekettlebell/time_kettlebell/src/pages/Timer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './pages.css'
 
@@ -11,30 +11,55 @@ import Button from '../components/Button'
 
 const Timer = () => {
 
-  let [ time, setTime ] = useState()
+  const [tempoEmMinutos, setTempoEmMinutos] = useState(0)
+  let [gameTime, setGameTime] = useState('00:00')
 
-  const TEMPO_TOTAL = 30;
-  let [tempoRestante, setTempoRestante] = useState(TEMPO_TOTAL)
-  
-  const startTimer = () => {
-    setTimeout(() => {
-        if (tempoRestante > 0) {
-            setTempoRestante(tempoRestante -= 1);
-        }
-    }, 1000)
-  }
+  console.log(`TEMPO EM MINUTOS: ${tempoEmMinutos}`)
 
-  const formataTempo = (time) => {
-      const minutes = Math.floor(time / 60);
-      let seconds = time % 60;
-      if (seconds < 10) {
-          seconds = `0${seconds}`;
+  useEffect(( ) => {
+    Stopwatch()
+    const Stopwatch = async () => {
+      
+      function TimeSeconds (time) {
+        return time * 60
       }
+      
+      function syncDelay(milliseconds){
+        var start = new Date().getTime();
+        var end=0;
+        while( (end-start) < milliseconds){
+            end = new Date().getTime();
+        }
+       } 
+      
+       const FormatTime = (time) => {
+        const minute = Math.floor(time / 60)
+        let seconds = time % 60
+        if( seconds < 10 ){
+          seconds = `0${seconds}`
+        }
+        console.clear()
+        console.log(`TIME: ${minute}:${seconds}`)
+        setGameTime(`${minute}:${seconds}`) 
+         
+      }
+      
+      let tempoEmSegundos = TimeSeconds(tempoEmMinutos)
+      console.log(`TEMPO EM SEGUNDOS: ${tempoEmSegundos}`)
+      
+      while(tempoEmSegundos > 0){
+        tempoEmSegundos -= 1
+        syncDelay(1000)
+        FormatTime(tempoEmSegundos)
+      }
+      
+    }
 
-      setTime(`${minutes}:${seconds}`);
+  },[tempoEmMinutos])
+
+  const changeTime = () => {
+    setTempoEmMinutos(1)
   }
-
-  formataTempo(tempoRestante)
 
   return (
   
@@ -44,10 +69,10 @@ const Timer = () => {
         <div className="container-operator">
           <div className="time-and-reps">
             <Repetitions> 100 </Repetitions>
-            <Time> { time } </Time>
+            <Time> { gameTime } </Time>
           </div>
           <div className="operation-button">
-            <Button className="child" onClick={startTimer()} > Start </Button>
+            <Button className="child" onClick={() => changeTime()}> Start </Button>
             <Button className="child" > Click </Button>
           </div>
         </div>
