6371d0620bdf8f4149b41ca2cff8a21537c8acad
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2022-06-28 21:57:58 -0300

    Site Timer Kettlebell: add function increment repetitions

--- site_timekettlebell/time_kettlebell/src/pages/Timer.jsx
+++ site_timekettlebell/time_kettlebell/src/pages/Timer.jsx
@@ -11,54 +11,67 @@ import Button from '../components/Button'
 
 const Timer = () => {
 
-  const [tempoEmMinutos, setTempoEmMinutos] = useState(0)
-  let [gameTime, setGameTime] = useState('00:00')
+  const [timeInSeconds, setTimeInSeconds] = useState(0)
+  // const [tempoEmMinutos, setTempoEmMinutos] = useState(0)
+  const [gameTime, setGameTime] = useState('00:00')
 
-  console.log(`TEMPO EM MINUTOS: ${tempoEmMinutos}`)
+  // console.log(`TEMPO EM MINUTOS: ${tempoEmMinutos}`)
 
-  useEffect(( ) => {
-    Stopwatch()
-    const Stopwatch = async () => {
+  function startStopWatch(time) {
       
-      function TimeSeconds (time) {
-        return time * 60
+    // function timeSeconds (timeInMinutes) {
+    //   return Math.round(timeInMinutes * 60)
+    // }
+
+    function syncDelay(){
+      var start = new Date().getTime();
+      var end=0;
+      while( (end-start) < 1000){
+          end = new Date().getTime();
       }
-      
-      function syncDelay(milliseconds){
-        var start = new Date().getTime();
-        var end=0;
-        while( (end-start) < milliseconds){
-            end = new Date().getTime();
-        }
-       } 
-      
-       const FormatTime = (time) => {
-        const minute = Math.floor(time / 60)
-        let seconds = time % 60
-        if( seconds < 10 ){
-          seconds = `0${seconds}`
-        }
-        console.clear()
-        console.log(`TIME: ${minute}:${seconds}`)
-        setGameTime(`${minute}:${seconds}`) 
-         
+    } 
+
+    const FormatTime = (time) => {
+      const minute = Math.floor(time / 60)
+      let seconds = time % 60
+      if( seconds < 10 ){
+        seconds = `0${seconds}`
       }
-      
-      let tempoEmSegundos = TimeSeconds(tempoEmMinutos)
-      console.log(`TEMPO EM SEGUNDOS: ${tempoEmSegundos}`)
-      
-      while(tempoEmSegundos > 0){
-        tempoEmSegundos -= 1
-        syncDelay(1000)
-        FormatTime(tempoEmSegundos)
-      }
-      
+      console.clear()
+      console.log(`TIME: ${minute}:${seconds}`)
+      setGameTime(`${minute}:${seconds}`)
     }
 
-  },[tempoEmMinutos])
+    let tempoEmSegundos = time
+    console.log(`TEMPO EM SEGUNDOS: ${tempoEmSegundos}`)
+    
+    while(tempoEmSegundos > 0){
+      
+      syncDelay()
+      FormatTime(tempoEmSegundos)
+      console.log(`TEMPO EM SEGUNDOSS: ${tempoEmSegundos}`)
+      tempoEmSegundos--         
+    }
+
+  }
+
+  useEffect(() => {
+     
+    startStopWatch(timeInSeconds)
+
+  },[timeInSeconds])
 
   const changeTime = () => {
-    setTempoEmMinutos(1)
+    setTimeInSeconds(5)
+  }
+
+  // repetitions
+
+  const [repetitions, setRepetitions] = useState(0)
+
+  const changeRepetitions = () => {
+    setRepetitions((prevState)=> prevState + 1)
+    console.log(`REPETIÇÕES: ${repetitions}`)
   }
 
   return (
@@ -68,12 +81,12 @@ const Timer = () => {
         <AthleteName> Thiago Carlos </AthleteName>
         <div className="container-operator">
           <div className="time-and-reps">
-            <Repetitions> 100 </Repetitions>
+            <Repetitions> { repetitions } </Repetitions>
             <Time> { gameTime } </Time>
           </div>
           <div className="operation-button">
-            <Button className="child" onClick={() => changeTime()}> Start </Button>
-            <Button className="child" > Click </Button>
+            <Button className="child" onClick={changeTime}> Start </Button>
+            <Button className="child" onClick={changeRepetitions}> Click </Button>
           </div>
         </div>
         <Footer />
