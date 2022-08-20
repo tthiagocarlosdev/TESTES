f2c2f901db2f44ce2b24a90ef0aacf798157444e
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2022-08-20 16:49:23 -0300

    Timer Kettlebell: construction of the repetitions box; repeats button, and footer. Start time button adjustment.

--- site_timekettlebell/time_kettlebell/src/components/Repetitions.jsx
+++ site_timekettlebell/time_kettlebell/src/components/Repetitions.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import { useState } from 'react';
+import Button from './Button'
 
-const Repetitions = ( { children } ) => {
+const Repetitions = ( ) => {
+  const [repetitions, setRepetitions] = useState(0);
+
+  const handleRepsClick = () => {
+    setRepetitions(repetitions + 1)
+  }
+
   return ( 
     <>
-      <div className="repetitions">
-        <h2> { children } </h2>
+      <div className="box-repetitions">
+        <h1> { repetitions } </h1>
+        <Button onClick={ handleRepsClick } > + 1 rep </Button>
       </div>
     </>
    );
--- site_timekettlebell/time_kettlebell/src/pages/Timer.jsx
+++ site_timekettlebell/time_kettlebell/src/pages/Timer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import './pages.css'
 
@@ -7,72 +7,8 @@ import Footer from '../components/Footer'
 import AthleteName from '../components/AthleteName'
 import Repetitions from '../components/Repetitions'
 import Time from '../components/Time'
-import Button from '../components/Button'
 
 const Timer = () => {
-
-  const [timeInSeconds, setTimeInSeconds] = useState(0)
-  // const [tempoEmMinutos, setTempoEmMinutos] = useState(0)
-  const [gameTime, setGameTime] = useState('00:00')
-
-  // console.log(`TEMPO EM MINUTOS: ${tempoEmMinutos}`)
-
-  function startStopWatch(time) {
-      
-    // function timeSeconds (timeInMinutes) {
-    //   return Math.round(timeInMinutes * 60)
-    // }
-
-    function syncDelay(){
-      var start = new Date().getTime();
-      var end=0;
-      while( (end-start) < 1000){
-          end = new Date().getTime();
-      }
-    } 
-
-    const FormatTime = (time) => {
-      const minute = Math.floor(time / 60)
-      let seconds = time % 60
-      if( seconds < 10 ){
-        seconds = `0${seconds}`
-      }
-      console.clear()
-      console.log(`TIME: ${minute}:${seconds}`)
-      setGameTime(`${minute}:${seconds}`)
-    }
-
-    let tempoEmSegundos = time
-    console.log(`TEMPO EM SEGUNDOS: ${tempoEmSegundos}`)
-    
-    while(tempoEmSegundos > 0){
-      
-      syncDelay()
-      FormatTime(tempoEmSegundos)
-      console.log(`TEMPO EM SEGUNDOSS: ${tempoEmSegundos}`)
-      tempoEmSegundos--         
-    }
-
-  }
-
-  useEffect(() => {
-     
-    startStopWatch(timeInSeconds)
-
-  },[timeInSeconds])
-
-  const changeTime = () => {
-    setTimeInSeconds(5)
-  }
-
-  // repetitions
-
-  const [repetitions, setRepetitions] = useState(0)
-
-  const changeRepetitions = () => {
-    setRepetitions((prevState)=> prevState + 1)
-    console.log(`REPETIÇÕES: ${repetitions}`)
-  }
 
   return (
   
@@ -81,8 +17,9 @@ const Timer = () => {
         <AthleteName> THIAGO CARLOS </AthleteName>
         <div className="box-time-reps">
           <Time/>
+          <Repetitions/>
         </div>
-        {/* <Footer /> */}
+        <Footer />
       </div>
 
    );
