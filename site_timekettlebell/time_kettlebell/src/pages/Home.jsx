03a3c93b58035599fd583f535d3637691eb92b48
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2022-08-23 23:34:54 -0300

    Timer Kettlebell: construcao da pagina de registro do atleta, com formulario contendo o nome do atleta, nome da prova e o peso do kettlebell. Falta criar funcionalidade de validacao e passar essas informacoes para  pagina timer.

--- site_timekettlebell/time_kettlebell/src/pages/Home.jsx
+++ site_timekettlebell/time_kettlebell/src/pages/Home.jsx
@@ -3,24 +3,32 @@ import { useNavigate } from 'react-router-dom'
 
 import Header from '../components/Header'
 import Button from '../components/Button'
+import Footer from '../components/Footer'
 
 import './pages.css'
 
 const Home = () => {
   let navigate = useNavigate()
+  
   return ( 
-    <>
+    <div className="container-home">
       <Header/>
-      <div className="container-buttons">
+        <div className="box-registration">
+          <p className="welcome" > Bem-vindo ao Campeonato de Kettlebell 2022 </p>
+          <p className="welcome"> Clique no botão abaixo para registrar o Atleta.</p>
+          <Button className="registration-button"onClick={() => navigate('/registration')} > Registrar Atleta </Button>
+        </div>
+      <Footer/>
+      {/* <div className="container-buttons">
         <div className="button-registration">
           <Button onClick={() => navigate('/registration')} > Registration </Button>
         </div>
         <div className="button-start">
           <Button onClick={() => navigate('/timer')} > Start </Button>
         </div>
-      </div>
+      </div> */}
       
-    </>
+    </div>
    );
 }
  
--- site_timekettlebell/time_kettlebell/src/pages/Registration.jsx
+++ site_timekettlebell/time_kettlebell/src/pages/Registration.jsx
@@ -1,10 +1,81 @@
-import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
+
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Button from '../components/Button'
 
 const Registration = () => {
+  let navigate = useNavigate()  
+
+  const [inputName, setInputName] = useState("")
+  const [inputStandard, setInputStandard] = useState("")
+  const [inputKettlebellWeight, setInputKettlebellWeight] = useState()
+
+  function handleInputName(e){
+    setInputName(e.target.value)
+  }
+
+  function handleInputStandard(e){
+    setInputStandard(e.target.value)
+  }
+
+  function handleInputKettlebellWeight(e){
+    setInputKettlebellWeight(e.target.value)
+  }
+
+  console.log(inputName, inputStandard, inputKettlebellWeight)
+
   return (
-    <>
-      <h1> Registration </h1>
-    </>
+    <div className="container-registration">
+      <Header/>
+        <div className="athlete-form">
+
+          <h1 className="title-athlete-form"> Registro do atleta: </h1>
+          <p className="text-athlete-form"> Digite as informações  abaixo: </p>
+
+          <div className="standard-form">
+            <label className="input" > Nome do Atleta: </label>
+            <input 
+              type="text"
+              onChange={handleInputName}
+              value={inputName}
+              className="input-athlete-name"
+              placeholder="Digite o nome..."
+            />  
+          </div>
+
+          <div className="standard-form">
+            <label className="input" > Prova: </label>
+            <input 
+              type="text"
+              onChange={handleInputStandard}
+              value={inputStandard}
+              className="input-athlete-test"
+              placeholder="Digite a prova..."
+            />  
+          </div>
+
+          <div className="standard-form">
+            <label className="input" > Peso Kettlebell: </label>
+            <input 
+              type="number"
+              onChange={handleInputKettlebellWeight}
+              value={inputKettlebellWeight}
+              className="input-kettlebell-weight"
+              placeholder="Digite o peso em kg..."
+            />  
+          </div>
+
+          <Button 
+            onClick={() => navigate('/timer')} 
+            className="load-test-page"
+          > Começar </Button>
+          
+        </div>
+        
+      <Footer/>
+    </div>
     );
 }
  
