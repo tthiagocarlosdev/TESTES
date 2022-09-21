020f04f1693241af6cbedc8c88032015e1aba1ac
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2022-09-21 16:59:43 -0300

    html-sites/snake_case: add conditions end string length.

--- html-sites/snake_case/script.js
+++ html-sites/snake_case/script.js
@@ -1,6 +1,7 @@
 function snakeCase(){
   const typed_text = document.querySelector('input#typed-text').value;
-  const final_result = document.querySelector('.final-result');
+  const string_final = document.querySelector('.string_final');
+  const string_lenght = document.querySelector('.string_lenght')
 
   let lowercaseText = typed_text.toLowerCase();
   let underlined_text = lowercaseText.replace(/ /gi, "_");
@@ -16,48 +17,59 @@ function snakeCase(){
   for( let counter = 0; counter < text_size; counter++ ){
     
     //conditions
-    let letter_e = underlined_text.charCodeAt(counter) >= 232 && underlined_text.charCodeAt(counter) <= 235;
     let special_characters_1 = underlined_text.charCodeAt(counter) >= 33 && underlined_text.charCodeAt(counter) <= 47;
     let special_characters_2 = underlined_text.charCodeAt(counter) >= 58 && underlined_text.charCodeAt(counter) <= 64;
-    let special_characters_3 = underlined_text.charCodeAt(counter) >= 123 && underlined_text.charCodeAt(counter) <= 191;
-    //letras apenas minusculas
-    let letter_A = underlined_text.charCodeAt(counter) >= 192 && underlined_text.charCodeAt(counter) <= 198;
-    let letters_Cc = underlined_text.charCodeAt(counter) === 199 || underlined_text.charCodeAt(counter) === 231;
-    let letter_E = underlined_text.charCodeAt(counter) >= 200 && underlined_text.charCodeAt(counter) <= 203;
-    let letter_I = underlined_text.charCodeAt(counter) >= 204 && underlined_text.charCodeAt(counter) <= 207;
-    let letter_D = underlined_text.charCodeAt(counter) === 240;
+    let special_characters_3 = underlined_text.charCodeAt(counter) >= 91 && underlined_text.charCodeAt(counter) <= 94 || underlined_text.charCodeAt(counter) === 96;
+    let special_characters_4= underlined_text.charCodeAt(counter) >= 123 && underlined_text.charCodeAt(counter) <= 191;
+
 
     
-    if (letter_e) {
-      array_without_accent[counter] = 'e'
-    } else if (special_characters_1) {
+    if (special_characters_1) {
       array_without_accent[counter] = ''
     } else if (special_characters_2) {
       array_without_accent[counter] = ''
     } else if (special_characters_3) {
       array_without_accent[counter] = ''
-    } else if (letter_A) {
-      array_without_accent[counter] = 'a'
-    } else if (letters_Cc) {
-      array_without_accent[counter] = 'c'
-    } else if (letter_E) {
-      array_without_accent[counter] = 'e'
-    } else if (letter_I) {
-      array_without_accent[counter] = 'i'
-    } else if (letter_D) {
-      array_without_accent[counter] = 'd'
-    }
+    } else if (special_characters_4) {
+      array_without_accent[counter] = '1'}
+    // } else if (letters_Cc) {
+    //   array_without_accent[counter] = 'c'
+    // } else if (letter_E) {
+    //   array_without_accent[counter] = 'e'
+    // } else if (letter_I) {
+    //   array_without_accent[counter] = 'i'
+    // } else if (letter_D) {
+    //   array_without_accent[counter] = 'd'
+    // }
 
   }
+  
 
   //criando a nova string a partir dos novos valores do array
   text_snake_case = array_without_accent.toString().replace(/,/gi, "");
 
-  final_result.innerHTML = text_snake_case;
+  string_final.innerHTML = text_snake_case;
+  string_lenght.innerHTML = text_size;
 }
 /*
 
 TABELA ASCII
 https://www.ime.usp.br/~glauber/html/acentos.htm
+special_characters_1: 33 a 47 - !"#$%&'()*+,-./
+special_characters_2: 58 a 64 - :;<=>?@
+special_characters_3: 91 a 96 - [\]^_` 95
+special_characters_4: 123 a 191 - {|}~¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·,¹º»¼½¾¿
+letter_a: 224 a 230 ou 256 - à á â ã ä å æ Ā
+letter_c: 231
+letter_e: 232 a 235 - è é ê ë 
+letter_i: 236 a 239 - ì í î ï
+letter_d: 240 - ð
+letter_n: 241 - ñ
+letter_o: 242 a 246 ou 248 - ò ó ô õ ö ø
+letter_x: 215 ou 247 - × ÷
+letter_u: 249 a 252 - ù ú û ü
+letter_y: 253 ou 255 - ý
+letter_b: 254 - þ
+
 
  */
