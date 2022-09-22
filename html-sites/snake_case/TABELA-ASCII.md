5c03eb79a48567e132f98aff3fb855d91121f756
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2022-09-21 22:20:17 -0300

    html-sites/snake_case: conditions refactoring, ascii table correction, lowercase only at end and final string length correction.

---
+++ html-sites/snake_case/TABELA-ASCII.md
@@ -0,0 +1,160 @@
+# TABELA ASCII
+
+[link table](https://www.matematica.pt/util/resumos/tabela-ascii.php)
+
+numbers: 48 a 57
+
+```tex
+0123456789
+```
+
+capital_letters: 65 a 90 
+
+```tex
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+```
+
+small_letters: 97 a 122
+
+```tex
+abcdefghijklmnopqrstuvwxyz
+```
+
+### Caracteres eliminados
+
+special_characters_1:  33 a 47 ou 58 a 64 ou 91 a 94 ou 96 ou 123 a 126
+
+```tex
+!"#$%&'()*+,-./:;<=>?@[\]^`{|}~
+```
+
+Obs.: _ é o número 95, mas não vamos colocar nas condicionais.
+
+special_characters_2:  163, 170, 186, 191, 174, 172, 189, 188, 161, 171, 187, 9617, 9618, 9619, 9474, 9508, 169, 9571, 9553, 9559, 9565, 165, 9488, 9492, 9524, 9516, 9500, 9472, 9532, 9562, 9556, 9577, 9574, 9568, 9552, 9580, 164, 9496, 9484, 9608, 9604, 166, 9600, 181, 254, 222, 175, 180, 95, 177, 8215, 190, 182, 167, 247, 184, 176, 168, 183, 185, 179, 178, 9632, 95
+
+```tex
+£ªº¿®¬½¼¡«»░▒▓│┤©╣║╗╝¥┐└┴┬├─┼╚╔╩╦╠═╬¤┘┌█▄¦▀µþÞ¯´ ±‗¾¶§÷¸°¨·¹³²■ 
+```
+
+letter_c: 199, 231, 162
+
+```tex
+Çç¢
+```
+
+
+
+
+
+
+
+letter_u: 129 ou 150 a 151 - üûù
+
+letter_e: 130 ou 136 a 138 - éêëè
+
+letter_a: 131 a 134 - âäàå
+
+letter_i: 139 a 141 - ïîì
+
+letter_A: 142 a 143 - ÄÅ
+
+letter_E: 144 - É
+
+letter_aeAE: 145 e 146 - æÆ
+
+letter_o: 147 a 149 - ôöò
+
+letter_y: 152 - ÿ
+
+letter_O: 153 - Ö
+
+letter_U: 154 - Ü
+
+letter_o: 155 ou 157 - øØ
+
+letter_x: 158 - ×
+
+letter_a: 160 - á
+
+letter_u: 161 ou 163 - ùú
+
+letter_o: 162 - ó
+
+letter_n: 164 a 165 - ñÑ
+
+letter_A: 181 a 183 - ÁÂÀ
+
+letter_a: 198 a 199 - ãÃ
+
+letter_d: 208 a 209 - ðÐ
+
+letter_E: 210 a 212 - ÊËÈ
+
+letter_I: 213 a 216 - ıÍÎÏ
+
+letter_i: 222 - Ì
+
+letter_O: 224 - Ó
+
+letter_sz: 225 - ß
+
+letter_O: 226 a 229 - ÔÒõÕ
+
+letter_U: 233 a 235 - ÚÛÙ
+
+letter_y: 236 a 237 - ýÝ
+
+
+
+```
+```
+
+```tex
+special_characters_1: 33 a 47 - !"#$%&'()*+,-./
+numbers: 48 a 57 - 0123456789
+special_characters_2: 58 a 64 - :;<=>?@
+capital_letters: 65 a 90 - ABCDEFGHIJKLMNOPQRSTUVWXYZ
+special_characters_3: 91 a 96 - [\]^_`
+small_letters: 97 a 122 - abcdefghijklmnopqrstuvwxyz
+special_characters_4: 123 a 127 - {|}~
+letter_c: 128 ou 135 - Çç
+letter_u: 129 ou 150 a 151 - üûù
+letter_e: 130 ou 136 a 138 - éêëè
+letter_a: 131 a 134 - âäàå
+letter_i: 139 a 141 - ïîì
+letter_A: 142 a 143 - ÄÅ
+letter_E: 144 - É
+letter_aeAE: 145 e 146 - æÆ
+letter_o: 147 a 149 - ôöò
+letter_y: 152 - ÿ
+letter_O: 153 - Ö
+letter_U: 154 - Ü
+letter_o: 155 ou 157 - øØ
+special_characters_5: 156 - £
+letter_x: 158 - ×
+letter_a: 160 - á
+letter_u: 161 ou 163 - ùú
+letter_o: 162 - ó
+letter_n: 164 a 165 - ñÑ
+special_characters_6: 166 a 180 ªº¿®¬½¼¡«»░▒▓│┤
+letter_A: 181 a 183 - ÁÂÀ
+special_characters_7: 184 a 188 - ©╣║╗╝
+letter_c: 189 - ¢
+special_characters_8: 190 a 197 - ¥┐└┴┬├─┼
+letter_a: 198 a 199 - ãÃ
+special_characters_9: 200 - 207 - ╚╔╩╦╠═╬¤
+letter_d: 208 a 209 - ðÐ
+letter_E: 210 a 212 - ÊËÈ
+letter_I: 213 a 216 - ıÍÎÏ
+special_characters_10: 217 a 223 - ┘┌█▄¦
+letter_i: 222 -	Ì
+special_characters_11: 223 - ▀
+letter_O: 224 - Ó
+letter_sz: 225 - ß
+letter_O: 226 a 229 - ÔÒõÕ
+special_characters_12: 230 a 232 - µþÞ
+letter_U: 233 a 235 - ÚÛÙ
+letter_y: 236 a 237 - ýÝ
+special_characters_13: 238 a 255 - ¯´ ±‗¾¶§÷¸°¨·¹³²■ 
+```
+
--- html-sites/snake_case/script.js
+++ html-sites/snake_case/script.js
@@ -1,13 +1,13 @@
 function snakeCase(){
   const typed_text = document.querySelector('input#typed-text').value;
   const string_final = document.querySelector('.string_final');
-  const string_lenght = document.querySelector('.string_lenght')
+  const string_size = document.querySelector('.string_size')
 
-  let lowercaseText = typed_text.toLowerCase();
-  let underlined_text = lowercaseText.replace(/ /gi, "_");
+  let underlined_text = typed_text.replace(/ /gi, "_");
   let text_size = underlined_text.length;
   let array_without_accent = [];
-  let text_snake_case = ''
+  let text_snake_case = '';
+  let array_numbers = [];
   
 
   //extraindo cada caracter da string para um array
@@ -17,59 +17,84 @@ function snakeCase(){
   for( let counter = 0; counter < text_size; counter++ ){
     
     //conditions
-    let special_characters_1 = underlined_text.charCodeAt(counter) >= 33 && underlined_text.charCodeAt(counter) <= 47;
-    let special_characters_2 = underlined_text.charCodeAt(counter) >= 58 && underlined_text.charCodeAt(counter) <= 64;
-    let special_characters_3 = underlined_text.charCodeAt(counter) >= 91 && underlined_text.charCodeAt(counter) <= 94 || underlined_text.charCodeAt(counter) === 96;
-    let special_characters_4= underlined_text.charCodeAt(counter) >= 123 && underlined_text.charCodeAt(counter) <= 191;
-
-
+    array_numbers.push(underlined_text.charCodeAt(counter)) 
     
-    if (special_characters_1) {
+    let numbers = underlined_text.charCodeAt(counter) >= 48 && underlined_text.charCodeAt(counter) <= 57;
+    let capital_letters = underlined_text.charCodeAt(counter) >= 65 && underlined_text.charCodeAt(counter) <= 90;
+    let small_letters = underlined_text.charCodeAt(counter) >= 97 && underlined_text.charCodeAt(counter) <= 122;
+    let underline = underlined_text.charCodeAt(counter) === 95
+    let letter_c = underlined_text.charCodeAt(counter) === 199 || underlined_text.charCodeAt(counter) === 231 || underlined_text.charCodeAt(counter) === 162;
+
+
+    if(numbers && capital_letters && small_letters && underline){
+      
+    } else if (letter_c){
+      array_without_accent[counter] = 'c'
+    } else if (!numbers && !capital_letters && !small_letters && !underline) {
       array_without_accent[counter] = ''
-    } else if (special_characters_2) {
-      array_without_accent[counter] = ''
-    } else if (special_characters_3) {
-      array_without_accent[counter] = ''
-    } else if (special_characters_4) {
-      array_without_accent[counter] = '1'}
-    // } else if (letters_Cc) {
-    //   array_without_accent[counter] = 'c'
-    // } else if (letter_E) {
-    //   array_without_accent[counter] = 'e'
-    // } else if (letter_I) {
-    //   array_without_accent[counter] = 'i'
-    // } else if (letter_D) {
-    //   array_without_accent[counter] = 'd'
-    // }
+    }
+    
 
   }
   
+  console.log(array_numbers);
 
   //criando a nova string a partir dos novos valores do array
-  text_snake_case = array_without_accent.toString().replace(/,/gi, "");
+  text_snake_case = array_without_accent.toString().replace(/,/gi, "").toLowerCase();
 
-  string_final.innerHTML = text_snake_case;
-  string_lenght.innerHTML = text_size;
+  string_final.innerHTML = `Final string: ${text_snake_case}`;
+  string_size.innerHTML = `String size: ${text_snake_case.length}`;
 }
 /*
 
 TABELA ASCII
-https://www.ime.usp.br/~glauber/html/acentos.htm
-special_characters_1: 33 a 47 - !"#$%&'()*+,-./
-special_characters_2: 58 a 64 - :;<=>?@
-special_characters_3: 91 a 96 - [\]^_` 95
-special_characters_4: 123 a 191 - {|}~¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·,¹º»¼½¾¿
-letter_a: 224 a 230 ou 256 - à á â ã ä å æ Ā
-letter_c: 231
-letter_e: 232 a 235 - è é ê ë 
-letter_i: 236 a 239 - ì í î ï
-letter_d: 240 - ð
-letter_n: 241 - ñ
-letter_o: 242 a 246 ou 248 - ò ó ô õ ö ø
-letter_x: 215 ou 247 - × ÷
-letter_u: 249 a 252 - ù ú û ü
-letter_y: 253 ou 255 - ý
-letter_b: 254 - þ
+https://www.matematica.pt/util/resumos/tabela-ascii.php
 
+special_characters_1: 33 a 47 ou 59 a 64 - !"#$%&'()*+,-./
+numbers: 48 a 57 - 0123456789
+special_characters_2: 58 a 64 - :;<=>?@
+capital_letters: 65 a 90 - ABCDEFGHIJKLMNOPQRSTUVWXYZ
+special_characters_3: 91 a 96 - [\]^_`
+small_letters: 97 a 122 - abcdefghijklmnopqrstuvwxyz
+special_characters_4: 123 a 127 - {|}~
+letter_c: 128 ou 135 - Çç
+letter_u: 129 ou 150 a 151 - üûù
+letter_e: 130 ou 136 a 138 - éêëè
+letter_a: 131 a 134 - âäàå
+letter_i: 139 a 141 - ïîì
+letter_A: 142 a 143 - ÄÅ
+letter_E: 144 - É
+letter_aeAE: 145 e 146 - æÆ
+letter_o: 147 a 149 - ôöò
+letter_y: 152 - ÿ
+letter_O: 153 - Ö
+letter_U: 154 - Ü
+letter_o: 155 ou 157 - øØ
+special_characters_5: 156 - £
+letter_x: 158 - ×
+letter_a: 160 - á
+letter_u: 161 ou 163 - ùú
+letter_o: 162 - ó
+letter_n: 164 a 165 - ñÑ
+special_characters_6: 166 a 180 ªº¿®¬½¼¡«»░▒▓│┤
+letter_A: 181 a 183 - ÁÂÀ
+special_characters_7: 184 a 188 - ©╣║╗╝
+letter_c: 189 - ¢
+special_characters_8: 190 a 197 - ¥┐└┴┬├─┼
+letter_a: 198 a 199 - ãÃ
+special_characters_9: 200 - 207 - ╚╔╩╦╠═╬¤
+letter_d: 208 a 209 - ðÐ
+letter_E: 210 a 212 - ÊËÈ
+letter_I: 213 a 216 - ıÍÎÏ
+special_characters_10: 217 a 223 - ┘┌█▄¦
+letter_i: 222 -	Ì
+special_characters_11: 223 - ▀
+letter_O: 224 - Ó
+letter_sz: 225 - ß
+letter_O: 226 a 229 - ÔÒõÕ
+special_characters_12: 230 a 232 - µþÞ
+letter_U: 233 a 235 - ÚÛÙ
+letter_y: 236 a 237 - ýÝ
+special_characters_13: 238 a 255 - ¯´ ±‗¾¶§÷¸°¨·¹³²■ 
 
  */
