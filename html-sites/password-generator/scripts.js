5ea8e6e58b4de74c1274ff85ee409e3f40cf4a6a
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2022-09-29 21:40:04 -0300

    html-sites/password-generator: Added modal, style buttons modal, create functions closeModal, openModal and copyToClipboard.

--- html-sites/password-generator/scripts.js
+++ html-sites/password-generator/scripts.js
@@ -1,11 +1,21 @@
+/* variable declaration */
 const modal = document.querySelector('.modal-password')
 const password = document.querySelector('#password')
+const numberOfCharacters = document.querySelector('#number_of_characters')
 
-password.value = `SENHA GERADA`
+let regexNumber = /\d/gi
+let itsNumber = false
 
+/* functions declaration */
+//function close modal
 function closeModal() {
   modal.classList.add('closeModal')
-  password.innerHTML = password.value
+}
+
+//function open modal
+function openModal(){
+  modal.classList.add('openModal')
+  modal.classList.remove('closeModal')
 }
 
 /* function button copy */
@@ -14,3 +24,42 @@ function copyToClipboard(){
     alert(`Copied text: ${password.value}`)
   }) 
 }
+
+/* function regular expression */
+function isRegularExpression(stringValue, regex){
+  return regex.test(stringValue) ? true : false
+}
+
+function passwordGenerator(){
+  console.log(numberOfCharacters.value)
+  itsNumber = isRegularExpression(numberOfCharacters.value, regexNumber)
+  console.log(itsNumber)
+
+  if (!itsNumber) {
+    alert('[ATENÇÃO!] Valor incorreto! Digite um número!')
+  } else if (Number(numberOfCharacters.value) < 8 || Number(numberOfCharacters.value) > 40) {
+    alert('[ATENÇÃO!] Valor incorreto! Digite um valor entre 8 e 40!')  
+  } else if (itsNumber && Number(numberOfCharacters.value) >= 8 && Number(numberOfCharacters.value) <= 40) {
+    password.innerHTML = numberOfCharacters.value
+    openModal()
+  }
+  
+}
+
+   
+   
+  /* variables */
+   
+   
+   
+
+   /*validations */
+  //  if(!itsNumber){
+  //    alert()
+  //  } else if (Number(numberOfCharacters) < 8 || Number(numberOfCharacters) > 40){
+  
+  //  } else {
+  //    modal.classList.add('active')
+  //    console.log(modal)
+  //    console.log(numberOfCharacters)
+  //  }
