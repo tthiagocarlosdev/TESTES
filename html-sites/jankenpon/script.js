ec6f42363acddf5e82a33c678d3ef19ed4c929c2
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2022-10-03 19:50:53 -0300

    Creation of counter and numberOfGames variables; Added NumberOfGames( ) function; Creation of score_human, score_computer, scoreHuman and scoreComputer variables. Added ScoreHuman( ) and ScoreComputer( ) functions; Creation of reset button and Added Reset( ) function; reset button positioned and styled.

--- html-sites/jankenpon/script.js
+++ html-sites/jankenpon/script.js
@@ -2,6 +2,11 @@ const humanPlay = document.querySelector('#human_play')
 const computerPlay = document.querySelector('#computer_play')
 const scoreWinner = document.querySelector('#score_winner')
 const numberOfGames = document.querySelector('#number_of_games')
+let counter = 0
+const scoreHuman = document.querySelector('#score_human')
+const scoreComputer = document.querySelector('#score_computer')
+let score_human = 0
+let score_computer = 0
 
 
 function getRandomIntInclusive(min, max){
@@ -10,7 +15,7 @@ function getRandomIntInclusive(min, max){
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-function computerChoise(choise){
+const ComputerChoise = (choise) => {
 
   let computerChoise = ""
 
@@ -33,55 +38,79 @@ const Winner = (choiseHuman, choiseComputer) => {
     winner = " "
   } else if(choiseHuman == "Stone" && choiseComputer == "Scissors"){
     winner = "Human"
+    ScoreHuman()
   } else if (choiseHuman == "Scissors" && choiseComputer == "Paper"){
     winner = "Human"
+    ScoreHuman()
   } else if (choiseHuman == "Paper" && choiseComputer == "Stone"){
     winner = "Human"
+    ScoreHuman()
   } else if(choiseComputer == "Stone" && choiseHuman == "Scissors"){
     winner = "Computer"
+    ScoreComputer()
   } else if (choiseComputer == "Scissors" && choiseHuman == "Paper"){
     winner = "Computer"
+    ScoreComputer()
   } else if (choiseComputer == "Paper" && choiseHuman == "Stone"){
     winner = "Computer"
+    ScoreComputer()
   } else if (choiseComputer == choiseHuman){
     winner = "A tie"
   }
 
-  console.log(winner)
   return winner
+}
+
+const NumberOfGames = (number) => {
+  numberOfGames.innerHTML = number
+}
+
+const ScoreHuman = () => {
+  ++score_human
+}
+
+const ScoreComputer = () => {
+  ++score_computer
 }
 
 function stone(){
   humanPlay.innerHTML = `Stone`
-  computerPlay.innerHTML = computerChoise(getRandomIntInclusive(1, 3))
-  printChoise(humanPlay.textContent, computerPlay.textContent)
+  computerPlay.innerHTML = ComputerChoise(getRandomIntInclusive(1, 3))
   scoreWinner.innerHTML = Winner(humanPlay.textContent, computerPlay.textContent)
-  numberOfGames.innerHTML = NumberOfGames(numbers)
+  NumberOfGames(++counter)
+  scoreHuman.innerHTML = score_human
+  scoreComputer.innerHTML = score_computer
+
 }
 
 function paper(){
   humanPlay.innerHTML = `Paper`
-  computerPlay.innerHTML = computerChoise(getRandomIntInclusive(1, 3))
-  printChoise(humanPlay.textContent, computerPlay.textContent)
+  computerPlay.innerHTML = ComputerChoise(getRandomIntInclusive(1, 3))
   scoreWinner.innerHTML = Winner(humanPlay.textContent, computerPlay.textContent)
+  NumberOfGames(++counter)
+  scoreHuman.innerHTML = score_human
+  scoreComputer.innerHTML = score_computer
+
 }
 
 function scissors(){
   humanPlay.innerHTML = `Scissors`
-  computerPlay.innerHTML = computerChoise(getRandomIntInclusive(1, 3))
-  printChoise(humanPlay.textContent, computerPlay.textContent)
+  computerPlay.innerHTML = ComputerChoise(getRandomIntInclusive(1, 3))
   scoreWinner.innerHTML = Winner(humanPlay.textContent, computerPlay.textContent)
+  NumberOfGames(++counter)
+  scoreHuman.innerHTML = score_human
+  scoreComputer.innerHTML = score_computer
+
 }
 
-function printChoise(a, b){
-  console.log(a)
-  console.log(b)
+const Reset = () => {
+  humanPlay.innerHTML = `******`
+  computerPlay.innerHTML = `******`
+  scoreWinner.innerHTML = `******`
+  counter = 0
+  score_human = 0
+  score_computer = 0
+  scoreHuman.innerHTML = score_human
+  scoreComputer.innerHTML = score_computer
+  NumberOfGames(counter)
 }
-
-
-
-function NumberOfGames(number){
-  number = number + 1
-  return number
-}
-
