8d95b1ef9d1ee7aebd712008bb5855fb3b9857ae
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2023-02-25 12:50:50 -0300

    random-applications: bug fixed

--- random-applications/passwordGenerator.js
+++ random-applications/passwordGenerator.js
@@ -4,15 +4,8 @@ const { basicFunctions } = require('./basicFunctions');
 
 //declaração de funções
 //Conditional (ternary) operator - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
-function isRegularExpression(stringValue, regex){
-    console.log(`string value: ${stringValue}`);
-    console.log(regex.test(stringValue))
-    // if( regex.test(stringValue) ){
-    //     return false;
-    // } else {
-    //     return true;
-    // }
-    return regex.test(stringValue) ? false : true
+function itsJustNumber(stringValue, regex){
+    return regex.RegExpNumber.test(stringValue) && !regex.RegRexLyrics.test(stringValue) ? true : false;
 };
 
 function errorMessage(){
@@ -26,30 +19,29 @@ function validateNumberOfCharacters(numberValue){
 //declaração de variáveis
 let numberOfCharactersTyped = 0;
 let itsNumber = true;
-let RegExpNumber = /[0-9]/gi;
+let regularExpressionObject = {
+    RegExpNumber : new RegExp("[0-9]", "i"),
+    RegRexLyrics : new RegExp("[a-z]", "i"),
+}
 let validNumber = true;
-    
+
 //entrada e validação de dados
 do {
-    // console.clear();
+    console.clear();
     basicFunctions.header("passwordGenerator");
     if ( !itsNumber || !validNumber ){
         errorMessage() 
     };
     numberOfCharactersTyped = input.question("Digite o número de caracteres [8 a 40]: ");
-    console.log(numberOfCharactersTyped);
-    itsNumber = isRegularExpression(numberOfCharactersTyped, RegExpNumber);
-    console.log(`itsNumber: ${itsNumber}`)
-    validNumber = validateNumberOfCharacters(numberOfCharactersTyped);
-    console.log(`validNumber: ${validNumber}`)
+    itsNumber = itsJustNumber(numberOfCharactersTyped, regularExpressionObject);
+    itsNumber == false ? validNumber = false : validNumber = validateNumberOfCharacters(numberOfCharactersTyped);
 
 } while ( !itsNumber || !validNumber );
 
 //saída de dados
-// console.clear();
+console.clear();
 basicFunctions.header("passwordGenerator");
 console.log(numberOfCharactersTyped);
-console.log(itsNumber);
 basicFunctions.line("passwordGenerator");
 
-//number Of Characters Typed - número de caracteres digitados
+//documentar do início
