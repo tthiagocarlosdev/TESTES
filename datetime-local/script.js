4cbef211b7c976ef66a0544d8e8002f3b6ec53cf
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2023-03-06 20:25:36 -0300

    datetime-local: como trabalhar com datas e horas usando o input datetime-local e constructor objeto Intl.DateTimeFormat

--- datetime-local/script.js
+++ datetime-local/script.js
@@ -1,16 +1,43 @@
-function dateAndTime(){
-    const date = document.querySelector('#date_time').value
-    const arrayDate = date.split("")
-    const year = teste(arrayDate, 3)
 
-    function teste(array, size){
-        let stringDate = ""
-        while (stringDate,length < array.length) {
-            stringDate += array[stringDate.length]
-        }
-        return stringDate
+function dateAndTime(){
+    let finalDate = document.querySelector('#resultador_final')    
+    const dateEntered = document.querySelector('#date_time').value
+    let resultFinal = ""
+
+    if (dateEntered.length == 0) {
+        const computerDate = new Date()
+        const actual = new Intl.DateTimeFormat("pt-br", {
+            dateStyle: "short", timeStyle: "short"
+        }).format(computerDate)
+
+        console.log(actual)
+
+        resultFinal = actual
+
+    } else {
+        const arrayDate = dateEntered.split("")
+        const year = arrayDate.slice(0, 4).toString().replace(/,/g, "") 
+        const month = arrayDate.slice(5, 7).toString().replace(/,/g, "")
+        const day = arrayDate.slice(8, 10).toString().replace(/,/g, "")
+        const hours = arrayDate.slice(11, 13).toString().replace(/,/g, "")
+        const minutes = arrayDate.slice(14, 16).toString().replace(/,/g, "")
+
+        resultFinal = `${day}/${month}/${year}, ${hours}:${minutes}`
+
+        console.log(dateEntered)
+
     }
 
-    console.log(date)
-    console.log(year)
+    finalDate.innerHTML = resultFinal
+    
 }
+
+/*
+    Documentação de input datetime-local e Intl.DateTimeFormat
+
+    * datetime-local -> <input> elementos do tipo datetime-local criam controles de entrada que permitem ao usuário inserir facilmente uma data e uma hora, incluindo ano, mês e dia, bem como a hora em horas e minutos.
+    * Intl.DateTimeFormat -> O objeto Intl.DateTimeFormat é um constructor para objetos que habilita o formato de data e hora no idioma padrão.
+
+https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/datetime-local#browser_compatibility
+https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
+*/
--- relatorio_de_servicos_externos/script.js
+++ relatorio_de_servicos_externos/script.js
@@ -4,15 +4,9 @@ function toView(){
     const pageData = {}
     pageData.serviceLocation = document.querySelector('.place_of_care_input').value
     pageData.dateAndTime = document.querySelector('#date_and_time_input').value
-    const year = 
-    const month =
-    const day =
-    const hour =
-    const minute =
+
     
    
-
-
     if(pageData.serviceLocation.length == 0){
         alert("Por favor, adicione o Local de Atendimento!")
     } else if (pageData.dateAndTime.length == 0) {
