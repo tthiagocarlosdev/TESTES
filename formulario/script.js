d96ec63015c17a3863c6a7041b5a9a7c11417411
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2024-03-23 21:11:49 -0300

    inicio do projeto formulariocom backend

---
+++ formulario/script.js
@@ -0,0 +1,7 @@
+// Adicionando evento de clique ao botão carregar
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Chamar a função para carregar as informações da unidade
+    alert("Teste Botão!")
+});
