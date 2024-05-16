fb278d746285853f574bd6db71ee1604bc5efd40
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2024-05-16 17:19:02 -0300

    add sistema_de_senhas

---
+++ sistema_de_senhas/script.js
@@ -0,0 +1,122 @@
+let senhasData = {};
+const janelaDialog = document.getElementById("modal");
+const datalist = document.getElementById('opcoes_de_sistemas')
+
+
+function loadJSON(filename) {
+    fetch(filename)
+        .then(response => response.json())
+        .then(data => {
+            senhasData = data;
+            alert("Arquivo JSON carregado com sucesso!");
+            console.log(senhasData);
+            listaSistemas(senhasData.senhas);
+            closeModal();
+        })
+        .catch(error => {
+            alert("Erro ao carregar o arquivo JSON.");
+            console.error(error);
+        });
+}
+
+
+// Função para fechar modal
+function closeModal() {
+    janelaDialog.close();
+};
+
+// Função para abrir modal
+function abrirModal(){
+    janelaDialog.showModal();
+};
+
+// Abre o modal quando a página é carregada
+document.addEventListener('DOMContentLoaded', (event) => {
+    abrirModal()
+});
+
+
+function listaSistemas(lista){
+    console.log(lista)
+    // Verifica se lista.senhas é uma matriz
+    if (Array.isArray(lista)) {
+        // Itera sobre a lista de sistemas
+        lista.forEach(sistema => {
+            // Cria um elemento de opção para cada sistema
+            const option = document.createElement('option');
+            option.value = sistema.nomeDoSistema;
+            datalist.appendChild(option);
+        });
+    } else {
+        console.error("A lista de sistemas não é uma matriz.");
+    }
+}
+
+
+// Função para buscar o sistema
+function buscarSistema() {
+    if (!senhasData) {
+        alert("Por favor, carregue um arquivo JSON primeiro.");
+        return;
+    }
+
+    const nomeDoSistema = document.getElementById('nome_do_sistema').value.trim().toLowerCase();
+    if (!nomeDoSistema) {
+        alert("Por favor, informe o nome do sistema.");
+        return;
+    }
+
+    // Procurar pelo sistema na lista de senhas
+    const sistema = senhasData.senhas.find(s => s.nomeDoSistema.toLowerCase() === nomeDoSistema);
+
+    if (!sistema) {
+        alert("Sistema não encontrado.");
+        return;
+    }
+
+    // Atualizar a UI com as informações do sistema
+    document.getElementById('linkSistema').href = sistema.endereco;
+    document.getElementById('linkSistema').textContent = sistema.nomeDoSistema;
+    document.getElementById('login').textContent = sistema.login;
+    document.getElementById('senha').textContent = sistema.senha;
+
+    adicionarBotaoCopiar('box_login_sistema', 'login');
+    adicionarBotaoCopiar('box_senha_sistema', 'senha');
+}
+
+
+//Função para copiar o texto para a área de transferência
+function copiarDados(element) {
+    const text = element.textContent.trim();
+    const tempTextArea = document.createElement('textarea');
+    tempTextArea.value = text;
+    document.body.appendChild(tempTextArea);
+    tempTextArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempTextArea);
+    alert(`${text} copiado para a área de transferência.`);
+}
+
+
+// Função para adicionar o botão "Copiar" após o elemento <p> em uma div específica
+function adicionarBotaoCopiar(nomeDaDiv, idDoSpan) {
+    // Obter o elemento <p> dentro da div especificada
+    const paragraphElement = document.querySelector(`#${nomeDaDiv} p`);
+
+    // Criar o botão "Copiar"
+    const copyButton = document.createElement('button');
+    copyButton.textContent = 'Copiar ';
+
+    // Adicionar a classe ao botão
+    copyButton.classList.add('botao_copiar');
+
+    // Adicionar evento de clique ao botão "Copiar"
+    copyButton.addEventListener('click', function() {
+        // Copiar o texto do span para a área de transferência
+        copiarDados(document.getElementById(idDoSpan));
+    });
+
+    // Adicionar o botão "Copiar" após o elemento <p>
+    paragraphElement.insertAdjacentElement('afterend', copyButton);
+}
+
