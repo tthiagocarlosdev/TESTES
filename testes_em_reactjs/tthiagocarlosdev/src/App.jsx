e06e398d1612f938e31ab40418af94764ebdba5e
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2024-06-29 11:23:18 +0100

    testes_em_reactjs/tthiagocarlosdev: Added component LineWithArrow

--- testes_em_reactjs/tthiagocarlosdev/src/App.jsx
+++ testes_em_reactjs/tthiagocarlosdev/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import SectionSubtitle from './components/SectionSubtitle/SectionSubtitle';
+import LineWithArrow from './components/LineWithArrow/LineWithArrow';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header/>
       <SectionSubtitle subtitle="Sobre mim" />
+      <LineWithArrow />
       <Footer/>
     </>
   )
