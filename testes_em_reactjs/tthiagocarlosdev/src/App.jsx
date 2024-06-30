01a41d7192ab9cc60eafecad4ae405b202d4655d
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2024-06-30 20:45:38 +0100

    testes_em_reactjs/tthiagocarlosdev: Added component Card, ProjectPaths and directory projectImages.

--- testes_em_reactjs/tthiagocarlosdev/src/App.jsx
+++ testes_em_reactjs/tthiagocarlosdev/src/App.jsx
@@ -3,6 +3,9 @@ import Footer from './components/Footer/Footer';
 import AboutMeSection from './components/AboutMeSection/AboutMeSection';
 import TechnologiesSection from './components/TechnologiesSection/TechnologiesSection'
 
+import Card from './components/Card/Card';
+import projects from './components/ProjectPaths/ProjectPaths';
+
 import './App.css';
 
 function App() {
@@ -12,6 +15,18 @@ function App() {
       <Header/>
       <AboutMeSection />
       <TechnologiesSection />
+      {projects.length > 0 ? (
+          projects.map((projeto, index) => (
+              <Card
+                  key={index}
+                  projectImage={projeto.projectImage}
+                  projectText={projeto.projectText}
+                  projectAddress={projeto.projectAddress}
+              />
+          ))
+      ) : (
+          <p className="paragrafoNenhumProjeto displayFlexCenterCenter">Nenhum projeto disponível no momento.</p>
+      )}
       <Footer/>
     </>
   )
